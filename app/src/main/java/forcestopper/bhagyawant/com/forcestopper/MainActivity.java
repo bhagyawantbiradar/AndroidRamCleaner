14bcf106a81d67fc3cb45174ff3dc3fee01c5010
Bhagyawant <bhagyawantbiradar@gmail.com>
2018-08-29 10:03:42 +0530

    Initial commit

---
+++ app/src/main/java/forcestopper/bhagyawant/com/forcestopper/MainActivity.java
@@ -0,0 +1,13 @@
+package forcestopper.bhagyawant.com.forcestopper;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
